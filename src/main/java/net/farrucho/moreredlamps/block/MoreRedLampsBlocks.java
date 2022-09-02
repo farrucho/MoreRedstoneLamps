1fbb71635588c4801e296b7ef91ddc6c31f950c0
farrucho <laranjofclash@gmail.com>
2022-09-03 00:00:19 +0100

    Fixed blocks mining speed with different pickaxes

--- src/main/java/net/farrucho/moreredlamps/block/MoreRedLampsBlocks.java
+++ src/main/java/net/farrucho/moreredlamps/block/MoreRedLampsBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.farrucho.moreredlamps.MoreRedLamps;
 import net.minecraft.block.Block;
+import net.minecraft.block.CraftingTableBlock;
 import net.minecraft.block.Material;
 import net.minecraft.block.RedstoneLampBlock;
 import net.minecraft.item.BlockItem;
@@ -20,26 +21,23 @@ public class MoreRedLampsBlocks {
     // else
     //      return 0
 
-    public static final Block RED_REDSTONE_LAMP = registerBlock("red_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block RED_REDSTONE_LAMP = registerBlock("red_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block YELLOW_REDSTONE_LAMP = registerBlock("yellow_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block YELLOW_REDSTONE_LAMP = registerBlock("yellow_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block BLUE_REDSTONE_LAMP = registerBlock("blue_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block BLUE_REDSTONE_LAMP = registerBlock("blue_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block BLACK_REDSTONE_LAMP = registerBlock("black_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block BLACK_REDSTONE_LAMP = registerBlock("black_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block WHITE_REDSTONE_LAMP = registerBlock("white_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block WHITE_REDSTONE_LAMP = registerBlock("white_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block GREEN_REDSTONE_LAMP = registerBlock("green_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block GREEN_REDSTONE_LAMP = registerBlock("green_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block ORANGE_REDSTONE_LAMP = registerBlock("orange_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block ORANGE_REDSTONE_LAMP = registerBlock("orange_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block PURPLE_REDSTONE_LAMP = registerBlock("purple_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
+    public static final Block PURPLE_REDSTONE_LAMP = registerBlock("purple_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
-    public static final Block PINK_REDSTONE_LAMP = registerBlock("pink_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
-
-
-
+    public static final Block PINK_REDSTONE_LAMP = registerBlock("pink_redstone_lamp", new RedstoneLampBlock(FabricBlockSettings.of(Material.REDSTONE_LAMP).hardness(0.3f).luminance(state -> state.get(RedstoneLampBlock.LIT) ? 15 : 0)), ItemGroup.REDSTONE);
 
 
     private static Block registerBlock(String name, Block block, ItemGroup group){
